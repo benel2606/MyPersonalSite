ac476b7dfd35441747275582757311721985a7fe
Ben Aharon <benel2606@gmail.com>
2020-05-04 15:00:18 +0300

    fixed broken image

--- src/App.js
+++ src/App.js
@@ -3,10 +3,12 @@ import MainInfoContainer from "./components/Home/MainInfo/MainInfoContainer";
 import AboutContainer from "./components/About/AboutContainer";
 import ExperienceConatiner from "./components/Experience/ExperienceContainer";
 import PortfolioConatainer from "./components/Portfolio/PortfolioContainer";
+import NavBar from "./components/NavBar/NavBar";
 
 const App = () => {
   return (
     <div>
+      <NavBar />
       <MainInfoContainer />
       <AboutContainer />
       <ExperienceConatiner />
